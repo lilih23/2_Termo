2109cfac6572df31e7042f1b4c525c1b5a669972
lilih23 <rodriguesferreira2612@gmail.com>
2026-08-12 10:49:58 -0300

    12/08/2026

---
+++ PROJETOS/aula5.js
@@ -0,0 +1,18 @@
+const readline = require("readline-sync");
+const nome = readline.question("Digite o nome do aluno: ");
+const disciplina = readline.question("Digite a disciplina: ");
+const nota1 = Number(readline.question("Digite a primeira nota: "));
+const nota2 = Number(readline.question("Digite a segunda nota: "));
+const faltas = Number(readline.question("Digite a quantidade de faltas: "));
+const media = (nota1 + nota2) / 2;
+const escola = readline.question("Digite o nome da escola: ");
+const ano_letivo = readline.question("Digite o ano letivo: ");
+console.log("\n--- RELATÓRIO DO ALUNO ---");
+console.log("Aluno:", nome);
+console.log("Disciplina:", disciplina);
+console.log("Nota 1:", nota1);
+console.log("Nota 2:", nota2);
+console.log("Média:", media);
+console.log("Faltas:", faltas);
+console.log("escola:", escola);
+console.log("ano letivo:", ano_letivo);
